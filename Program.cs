59495ea5add9d256a2f4965ab59fd84c2ff84665
Alexander Glotov <glob.nsk@mail.ru>
2024-05-30 17:04:58 +0300

    Create array elms console input

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Создаем ввод с консоли элементов массива
+// 1. Вводим количество элементов массива
+Console.WriteLine("Введите количество элементов массива");
+string k = Console.ReadLine();
+int n = Convert.ToInt32(k);
+//Console.WriteLine(n);
+// 2. Создаем пустой str массив и заполняем его значениями
+string [] arr_str = new string [n];
+int i=0;
+while (i<n)
+{
+    Console.WriteLine ("Введите  " + (i+1) + "-й элемент массива");
+    arr_str[i] = Console.ReadLine ();
+    Console.WriteLine(arr_str[i]);
+    i+=1;
+}
